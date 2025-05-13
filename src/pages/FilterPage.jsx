a3a1bf12f37705edc0e2a2c0b45365251d0d9a9b
jieun0240 <128780183+jieun0240@users.noreply.github.com>
2025-05-13 19:01:29 +0900

    feat: css폴더 추가

--- src/pages/FilterPage.jsx
+++ src/pages/FilterPage.jsx
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const filters = ["가오", "감성", "재미"];
 
 function FilterPage() {
     return (
