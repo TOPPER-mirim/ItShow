7c26f938bbf8031c98973ada43735b10cb44532c
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-05-14 11:49:14 +0900

    start page ipad 프론트 구현

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -9,6 +9,11 @@ function StartPage() {
                 alt="Center"
                 className="background-image"
             />
+            <img
+                src="/images/start-background-2-ipad.png"
+                alt="iPad Background"
+                className="ipad-background-image"
+                />
 
             <img
                 src="/images/logo.png"  
@@ -23,17 +28,36 @@ function StartPage() {
             />
 
             <img
+                src="/images/line-ipad.png"  
+                alt="Center"
+                className="line-ipad-image"
+            />
+
+
+            <img
                 src="/images/ticket-make-button.png"  
                 alt="Center"
                 className="make-ticket-button-image"
             />
 
+            <img
+                src="/images/ticket-make-button-ipad.png"  
+                alt=""
+                className="make-ticket-button-image-ipad"
+            />
             
             <img
              src="/images/see-ticket-button.png"  
                 alt="Center"
                 className="see-ticket-button-image"
             />
+
+            <img
+                src="/images/see-ticket-button-ipad.png"  
+                alt="Center"
+                className="make-ticket-button-image-ipad"
+            />
+
 
         </div>
     );
