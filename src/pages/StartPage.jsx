0cefae1992b862602e6ce1bdb1fc42ad13e53313
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-05-14 09:37:15 +0900

    startpage desktop 프론트 구현

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,9 +1,36 @@
-
+import '../styles/StartPage.css'; // CSS 파일 임포트
 
 function StartPage() {
     return (
-        <div>Start Page</div>
+        <div className="start-page">
+            <img
+                src="/images/logo.png"  
+                alt="Center"
+                className="center-image"
+            />
+
+            <img
+                src="/images/line.png"  
+                alt="Center"
+                className="line-image"
+            />
+
+            <img
+                src="/images/ticket-make-button.png"  
+                alt="Center"
+                className="make-ticket-button-image"
+            />
+
+            
+            <img
+             src="/images/see-ticket-button.png"  
+                alt="Center"
+                className="see-ticket-button-image"
+            />
+
+        </div>
     );
 }
+
 
 export default StartPage;
