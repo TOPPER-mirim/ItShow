03254eee4457e4c7e5e058cc50a78e8f76ba90dd
jieun0240 <128780183+jieun0240@users.noreply.github.com>
2025-05-15 16:48:16 +0900

    feat: filter페이지 추가

---
+++ src/components/FilterCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FilterCard = ({ name, image, description, example, onClick, className, bgimage }) => {
+    return (
+        <div className={`filter-card ${name}`} onClick={onClick}>
+            <img src={bgimage} alt="backgroudImg" className="bgImg"/>
+            <div className="filter-logo">
+                <img src={image} className="filter-icon" alt={`${name} 아이콘`} />
+                <p className="filter-name">{name}</p>
+            </div>
+
+            <div className="filter-description-container">
+                <div className="description">{description}</div>
+                <div className="ex">{example}</div>
+            </div>
+        </div>
+    );
+};
+
+export default FilterCard;
