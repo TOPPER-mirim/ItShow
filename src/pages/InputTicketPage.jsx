97547efa02a379a8550ade4b7999affdbcb6a91f
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-05-19 00:05:29 +0900

    InputTicketPage desktop 포론트 구현

--- src/pages/InputTicketPage.jsx
+++ src/pages/InputTicketPage.jsx
@@ -1,7 +1,59 @@
+import React from "react";
+// import Button from "../components/Button";
+import '../styles/InputTicketPage.css'; 
+
 
 function InputTicketPage() {
+
+
     return (
-        <div>View Tickets Page</div>
+        <div className="input-ticket-page">
+            <h1 className="title">이야기를 들려주세요!</h1>
+
+
+            <div className="ticket-wrapper">
+            <img
+                src="/images/input-ticket.png"
+                alt="샘플 이미지"
+                className="main-image"
+            />
+
+            
+                <input
+                    type="text"
+                    placeholder="자신의 고민이나 하고 싶은 말을 적어주세요. (최대 200자)"
+                    className="worry-input"
+                />
+
+            <img
+                src="/images/input-line.png"
+                alt="선"
+                className="line-image"
+            />
+
+
+                <input
+                    type="text"
+                    placeholder="닉네임을 적어주세요."
+                    className="nickname-input"
+                />
+
+            </div>
+
+            
+            <img
+                src="/images/filter-select-button.png"
+                alt="샘플 이미지"
+                className="button-image"
+            />
+
+            
+
+        {/*             
+            <Button onClick={handleButtonClick} size="small" variant="green">
+                필터 선택 하러 가기
+            </Button> */}
+        </div>
     );
 }
 
