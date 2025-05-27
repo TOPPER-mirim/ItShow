197dfe0eb74e5137b39254a81f7ec1aff50f31c3
jieun0240 <128780183+jieun0240@users.noreply.github.com>
2025-05-27 19:57:53 +0900

    feat: 프레임 설정 및 변경 기능 추가

---
+++ src/components/Frame1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Frame.css";
+
+const Frame1 = ({ fillColor = "black" }) => {
+    
+    return (
+        <svg width="949" height="370" viewBox="0 0 949 370" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect y="1" width="722.337" height="368.268" rx="30" fill={fillColor} />
+            <path d="M722 52V317" stroke="white" strokeDasharray="23 23" />
+            <rect x="722" width="227" height="369" rx="30" fill={fillColor} />
+        </svg>
+    );
+};
+
+export default Frame1;
--- src/pages/MakeTicketPage.jsx
+++ src/pages/MakeTicketPage.jsx
@@ -7,6 +7,10 @@ import TicketPreview from "../components/TicketPreview";
 import MakePaletter from "../components/MakePalette";
 import SinglePaletter from "../components/SinglePaletter";
 
+import Frame1 from "../components/Frame1";
+import Frame2 from "../components/Frame2";
+import Frame3 from "../components/Frame3";
+
 const MakeTicketPage = () => {
     const location = useLocation();
     const { nickname, content, filter } = location.state || {};
@@ -14,25 +18,41 @@ const MakeTicketPage = () => {
     const filterMap = {
         감성: "../images/필터-감성.svg",
         가오: "../images/필터-가오.svg",
-        개그: "../images/필터-개그.svg"
+        재미: "../images/필터-개그.svg"
     };
 
     const logoUrl = filterMap[filter];
-    const [filterColor, setFilterColor] = useState("#000000");
+    const [fillColor, setFillColor] = useState("#000000");
+
+    const [selectedFrame, setSelectedFrame] = useState(1); // default: Frame1
+
+    const handleFrameClick = (frameNumber) => {
+        setSelectedFrame(frameNumber);
+    };
 
     return (
         <div className="makeTicket-container">
 
-            <TicketPreview logoImgUrl={logoUrl} filterColor={filterColor}></TicketPreview>
+            <TicketPreview
+                logoImgUrl={logoUrl}
+                fillColor={fillColor}
+                frameIndex={selectedFrame}
+            />
 
             <MakePaletter title="프레임">
-                <SinglePaletter imageUrl="../images/none.svg" onClick={() => setFilterColor("#D9D9D9")} />
-                <SinglePaletter imageUrl="../images/pink.png" onClick={() => setFilterColor("#FFC1C1")} />
-                <SinglePaletter imageUrl="../images/yellow.png" onClick={() => setFilterColor("#FEC730")} />
-                <SinglePaletter imageUrl="../images/green.png" onClick={() => setFilterColor("#9CD69D")} />
-                <SinglePaletter imageUrl="../images/babyblue.png" onClick={() => setFilterColor("#DFECF2")} />
-                <SinglePaletter imageUrl="../images/blue.png" onClick={() => setFilterColor("#225268")} />
-                <SinglePaletter imageUrl="../images/black.png" onClick={() => setFilterColor("#000000")} />
+                <SinglePaletter imageUrl="../images/프레임1.png" onClick={() => handleFrameClick(1)} />
+                <SinglePaletter imageUrl="../images/프레임2.png" onClick={() => handleFrameClick(2)} />
+                <SinglePaletter imageUrl="../images/프레임3.png" onClick={() => handleFrameClick(3)} />
+            </MakePaletter>
+
+            <MakePaletter title="단색 배경">
+                <SinglePaletter imageUrl="../images/none.svg" onClick={() => setFillColor("#D9D9D9")} />
+                <SinglePaletter imageUrl="../images/pink.png" onClick={() => setFillColor("#FFC1C1")} />
+                <SinglePaletter imageUrl="../images/yellow.png" onClick={() => setFillColor("#FEC730")} />
+                <SinglePaletter imageUrl="../images/green.png" onClick={() => setFillColor("#9CD69D")} />
+                <SinglePaletter imageUrl="../images/babyblue.png" onClick={() => setFillColor("#DFECF2")} />
+                <SinglePaletter imageUrl="../images/blue.png" onClick={() => setFillColor("#225268")} />
+                <SinglePaletter imageUrl="../images/black.png" onClick={() => setFillColor("#000000")} />
             </MakePaletter>
 
             <Button size="big">럭키 티켓 출력하기</Button>
