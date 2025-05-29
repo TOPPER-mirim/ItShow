4975df80a12bdb61a6f130f14c206e850df1acb7
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-05-29 18:44:20 +0900

    페이지 연결 및 배경이미지 설정

--- src/pages/TicketsStylePage.jsx
+++ src/pages/TicketsStylePage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import '../styles/Tickets.css';
-
+import { Link } from "react-router-dom"; 
 
 function TicketsStylePage() {
     return (
         <div className="style-page">
+             <Link to="/tickets">
          <img
                 src="/images/style-font.png"  
                 alt="Center"
                 className="style-font-image"
             />
+            </Link>
     </div>
     );
 }
