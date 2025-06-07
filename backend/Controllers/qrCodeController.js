e17cec64c26a604ac1387fbf479f623a6f17e6c8
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-06-07 23:24:29 +0900

    사진이 없으면 큐알 생성하지 못 하도록 구현

--- backend/Controllers/qrCodeController.js
+++ backend/Controllers/qrCodeController.js
@@ -1,5 +1,7 @@
 import { models } from "../Models/index.js";
 import QRCode from "qrcode";
+import fs from "fs/promises";
+import path from "path";
 
 const qrCodeController = {
   // 큐알 코드 반환
@@ -16,6 +18,15 @@ const qrCodeController = {
       // DB에 저장된 경로
       const dbImgPath = imgData.img; //img: Image 테이블의 컬럼 이름
 
+      // 실제 서버 경로로 변환
+      const actualImgPath = path.join(process.cwd(), "backend", dbImgPath);
+
+      try {
+        await fs.access(actualImgPath);
+      } catch {
+        return res.status(404).send("이미지 파일이 존재하지 않습니다.");
+      }
+
       // 서버 주소 포함해서 전체 URL 구성 (프론트에서도 접근 가능하게)
       // encodeURI: URL에 들어가면 안 되는 문자들을 인코딩해주는 함수 (한글 포함)
       const imageURL = `${req.protocol}://${req.get("host")}/${encodeURI(dbImgPath)}`;
@@ -27,7 +38,7 @@ const qrCodeController = {
       res.json({ qrCode: qrcode, originalImagePath: imageURL });
     } catch (error) {
       console.error("QR 코드 생성 오류:", error);
-      res.status(500).json({ error: "QR 코드 생성 실패" });
+      res.status(500).json({ error: "QR 코드 생성 실패", detail: error.message });
     }
   },
 };
