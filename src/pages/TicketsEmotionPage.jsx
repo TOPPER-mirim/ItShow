030ead1bb426fd11b3e4dcd9b60820f52b42ea3b
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-06-16 14:30:59 +0900

    티켓북 티켓 종류 별로 티켓 보기 api 연결

--- src/pages/TicketsEmotionPage.jsx
+++ src/pages/TicketsEmotionPage.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/Tickets.css';
-import { Link } from "react-router-dom"; 
-
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function TicketsEmotionPage() {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        const fetchEmotionTickets = async () => {
+            try {
+                const res = await axios.post("http://54.180.152.171:3000/filterImages", {
+                    filterStr: "감성",
+                });
+                setImages(res.data);
+            } catch (error) {
+                console.error("감성 티켓 불러오기 실패:", error);
+            }
+        };
+
+        fetchEmotionTickets();
+    }, []);
+
     return (
         <div className="emotion-page">
-         <Link to="/tickets"> {/* 원하는 경로로 */}
-                <img
-                    src="/images/emotion-font.png"  
-                    alt="Center"
-                    className="emotion-font-image"
-                />
-            </Link>
-    </div>
+            <div className="content-container">
+                <Link to="/tickets">
+                    <img
+                        src="/images/emotion-font.png"
+                        alt="Center"
+                        className="emotion-font-image"
+                    />
+                </Link>
+
+                <div className="ticket-image-list">
+                    {images.map((img) => (
+                        <div key={img.id} className="ticket-image-item">
+                            <img src={img.imageUrl} alt={`감성 티켓 ${img.id}`} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/TicketsFunnyPage.jsx
+++ src/pages/TicketsFunnyPage.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/Tickets.css';
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function TicketsFunnyPage() {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        const fetchFunnyTickets = async () => {
+            try {
+                const res = await axios.post("http://54.180.152.171:3000/filterImages", {
+                    filterStr: "개그",
+                });
+                setImages(res.data);
+            } catch (error) {
+                console.error("개그 티켓 불러오기 실패:", error);
+            }
+        };
+
+        fetchFunnyTickets();
+    }, []);
+
     return (
         <div className="funny-page">
-              <Link to="/tickets">
-         <img
-                src="/images/funny-font.png"  
-                alt="Center"
-                className="funny-font-image"
-            />
-    </Link>
-    </div>
+            <div className="content-container">
+                <Link to="/tickets">
+                    <img
+                        src="/images/funny-font.png"
+                        alt="Funny"
+                        className="funny-font-image"
+                    />
+                </Link>
+
+                <div className="ticket-image-list">
+                    {images.map((img) => (
+                        <div key={img.id} className="ticket-image-item">
+                            <img src={img.imageUrl} alt={`개그 티켓 ${img.id}`} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 }
 
