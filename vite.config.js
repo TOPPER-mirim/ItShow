2a3a0f54400cb1efea6e831b088070318c8cb02e
jieun0240 <128780183+jieun0240@users.noreply.github.com>
2025-06-21 08:14:21 +0900

    이미지 압축 코드 추가

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // 백엔드 서버 주소
+        target: 'https://lucky-ticket.mirim-it-show.site',
         changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
